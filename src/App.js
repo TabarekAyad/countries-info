4250a52e68fd5cb08d9cf9a056bcd37d817c151f
unknown <tabarek.ayad@gmail.com>
2023-01-14 22:08:23 +0300

    integrated the api, viewed some content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './app.css';
+import { Routes, Route } from 'react-router-dom';
+import AllCountries  from './components/AllCountries/AllCountries';
+import CountryInfo  from './components/CountryInfo/CountryInfo';
 
-function App() {
-  return <h1>Hello</h1>;
+function App() {  
+  return(
+    <>
+      <header className="header">
+        <div className="container">
+        <h5>Where in the world?</h5>
+        </div>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<AllCountries />} />
+          <Route path='/country/:countryName' element={<CountryInfo />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
